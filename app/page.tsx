dddc57c65d35250bdb438ca1525c2f0a91a632a0
Chauhan-Ujjawal <ujjaval765@gmail.com>
2025-06-30 02:41:10 +0530

    Fix: escaped apostrophes for Vercel build

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,8 @@ export default function Home() {
           <i className="nes-bcrikko text-4xl bounce-brick" />
           <div className="absolute -top-6 left-[1.3rem] nes-balloon from-left is-dark text-black w-full max-w-[75rem] overflow-hidden">
             <p className="text-center typing-text text-xs sm:text-base">
-              👾 Hey There! I'm Ujjawal, Simple. Sharp. Slightly Obsessed With Details
+              👾 Hey There! I&apos;m Ujjawal, Simple. Sharp. Slightly Obsessed
+              With Details
             </p>
           </div>
         </div>
@@ -20,10 +21,16 @@ export default function Home() {
 
       {/* Intro Box */}
       <div className="nes-container with-title bg-blue-200 w-full max-w-[45rem] mx-auto mt-24 mb-20">
-        <p className="title text-xs sm:text-base">Intro Box. Main Character Energy Inside!</p>
+        <p className="title text-xs sm:text-base">
+          Intro Box. Main Character Energy Inside!
+        </p>
         <div className="p-6 space-y-6">
           <p className="text-sm sm:text-base text-center">
-            A creative technologist passionate about crafting smart, user-friendly experiences. I work across machine learning, UI/UX design, data science, frontend development, and MERN stack. Whether it's building sleek interfaces or training models, I love blending data, design, and development to bring bold ideas to life.
+            A creative technologist passionate about crafting smart,
+            user-friendly experiences. I work across machine learning, UI/UX
+            design, data science, frontend development, and MERN stack. Whether
+            it&apos;s building sleek interfaces or training models, I love blending
+            data, design, and development to bring bold ideas to life.
           </p>
         </div>
       </div>
@@ -45,13 +52,27 @@ export default function Home() {
               <h1 className="text-sm sm:text-base">These Are My Skills</h1>
             </div>
             <div className="grid grid-cols-2 sm:grid-cols-3 gap-4 justify-items-center mt-6">
-              <a href="#" className="nes-badge"><span className="is-dark">UI/UX</span></a>
-              <a href="#" className="nes-badge"><span className="is-primary">Frontend Dev.</span></a>
-              <a href="#" className="nes-badge"><span className="is-success">Data Science</span></a>
-              <a href="#" className="nes-badge"><span className="is-warning">ML</span></a>
-              <a href="#" className="nes-badge"><span className="is-error">Web Dev</span></a>
-              <a href="#" className="nes-badge"><span className="is-dark">Programming</span></a>
-              <a href="#" className="nes-badge"><span className="is-success">MERN</span></a>
+              <a href="#" className="nes-badge">
+                <span className="is-dark">UI/UX</span>
+              </a>
+              <a href="#" className="nes-badge">
+                <span className="is-primary">Frontend Dev.</span>
+              </a>
+              <a href="#" className="nes-badge">
+                <span className="is-success">Data Science</span>
+              </a>
+              <a href="#" className="nes-badge">
+                <span className="is-warning">ML</span>
+              </a>
+              <a href="#" className="nes-badge">
+                <span className="is-error">Web Dev</span>
+              </a>
+              <a href="#" className="nes-badge">
+                <span className="is-dark">Programming</span>
+              </a>
+              <a href="#" className="nes-badge">
+                <span className="is-success">MERN</span>
+              </a>
             </div>
           </div>
         </div>
@@ -66,15 +87,27 @@ export default function Home() {
               <h1 className="bold-white-outline text-center">Design</h1>
               <button className="nes-btn is-primary">Figma</button>
               <div className="grid grid-cols-2 sm:grid-cols-3 gap-4 mt-6">
-                <span className="nes-badge"><span className="is-dark">UI/UX</span></span>
-                <span className="nes-badge"><span className="is-primary">Wireframing</span></span>
-                <span className="nes-badge"><span className="is-warning">Prototyping</span></span>
-                <span className="nes-badge"><span className="is-success">IxDesign</span></span>
+                <span className="nes-badge">
+                  <span className="is-dark">UI/UX</span>
+                </span>
+                <span className="nes-badge">
+                  <span className="is-primary">Wireframing</span>
+                </span>
+                <span className="nes-badge">
+                  <span className="is-warning">Prototyping</span>
+                </span>
+                <span className="nes-badge">
+                  <span className="is-success">IxDesign</span>
+                </span>
               </div>
               <button className="nes-btn is-primary">Canva</button>
               <div className="grid grid-cols-2 sm:grid-cols-3 gap-4 mt-6">
-                <span className="nes-badge"><span className="is-success">Vis.Creator</span></span>
-                <span className="nes-badge"><span className="is-error">GraphicDes.</span></span>
+                <span className="nes-badge">
+                  <span className="is-success">Vis.Creator</span>
+                </span>
+                <span className="nes-badge">
+                  <span className="is-error">GraphicDes.</span>
+                </span>
               </div>
             </div>
           </div>
@@ -82,20 +115,38 @@ export default function Home() {
           {/* Box 2: Frontend Development */}
           <div className="nes-container is-rounded bg-[#797ea0] w-full max-w-[40rem]">
             <div className="p-6 space-y-6">
-              <h1 className="bold-white-outline text-center">Frontend Development</h1>
+              <h1 className="bold-white-outline text-center">
+                Frontend Development
+              </h1>
               <button className="nes-btn is-primary">Languages</button>
               <div className="grid grid-cols-2 sm:grid-cols-3 gap-4 mt-6">
-                <span className="nes-badge"><span className="is-dark">HTML</span></span>
-                <span className="nes-badge"><span className="is-primary">CSS</span></span>
-                <span className="nes-badge"><span className="is-warning">JavaScript</span></span>
+                <span className="nes-badge">
+                  <span className="is-dark">HTML</span>
+                </span>
+                <span className="nes-badge">
+                  <span className="is-primary">CSS</span>
+                </span>
+                <span className="nes-badge">
+                  <span className="is-warning">JavaScript</span>
+                </span>
               </div>
               <button className="nes-btn is-primary">Frameworks</button>
               <div className="grid grid-cols-2 sm:grid-cols-3 gap-4 mt-6">
-                <span className="nes-badge"><span className="is-success">Tailwind</span></span>
-                <span className="nes-badge"><span className="is-error">React</span></span>
-                <span className="nes-badge"><span className="is-dark">NES.css</span></span>
-                <span className="nes-badge"><span className="is-primary">Next.js</span></span>
-                <span className="nes-badge"><span className="is-warning">Bootstrap</span></span>
+                <span className="nes-badge">
+                  <span className="is-success">Tailwind</span>
+                </span>
+                <span className="nes-badge">
+                  <span className="is-error">React</span>
+                </span>
+                <span className="nes-badge">
+                  <span className="is-dark">NES.css</span>
+                </span>
+                <span className="nes-badge">
+                  <span className="is-primary">Next.js</span>
+                </span>
+                <span className="nes-badge">
+                  <span className="is-warning">Bootstrap</span>
+                </span>
               </div>
             </div>
           </div>
@@ -103,19 +154,35 @@ export default function Home() {
           {/* Box 3: ML and Data Science */}
           <div className="nes-container is-rounded bg-[#797ea0] w-full max-w-[40rem]">
             <div className="p-6 space-y-6">
-              <h1 className="bold-white-outline text-center">Machine Learning and Data Science</h1>
+              <h1 className="bold-white-outline text-center">
+                Machine Learning and Data Science
+              </h1>
               <button className="nes-btn is-primary">Languages</button>
               <div className="grid grid-cols-2 sm:grid-cols-3 gap-4 mt-6">
-                <span className="nes-badge"><span className="is-dark">Python</span></span>
-                <span className="nes-badge"><span className="is-primary">SQL</span></span>
+                <span className="nes-badge">
+                  <span className="is-dark">Python</span>
+                </span>
+                <span className="nes-badge">
+                  <span className="is-primary">SQL</span>
+                </span>
               </div>
               <button className="nes-btn is-primary">Libraries</button>
               <div className="grid grid-cols-2 sm:grid-cols-3 gap-4 mt-6">
-                <span className="nes-badge"><span className="is-success">TensorFlow</span></span>
-                <span className="nes-badge"><span className="is-error">Pandas</span></span>
-                <span className="nes-badge"><span className="is-dark">NumPy</span></span>
-                <span className="nes-badge"><span className="is-primary">Seaborn</span></span>
-                <span className="nes-badge"><span className="is-warning">Matplotlib</span></span>
+                <span className="nes-badge">
+                  <span className="is-success">TensorFlow</span>
+                </span>
+                <span className="nes-badge">
+                  <span className="is-error">Pandas</span>
+                </span>
+                <span className="nes-badge">
+                  <span className="is-dark">NumPy</span>
+                </span>
+                <span className="nes-badge">
+                  <span className="is-primary">Seaborn</span>
+                </span>
+                <span className="nes-badge">
+                  <span className="is-warning">Matplotlib</span>
+                </span>
               </div>
             </div>
           </div>
@@ -123,16 +190,26 @@ export default function Home() {
           {/* Box 4: General Development */}
           <div className="nes-container is-rounded bg-[#797ea0] w-full max-w-[40rem]">
             <div className="p-6 space-y-6">
-              <h1 className="bold-white-outline text-center">General Development</h1>
+              <h1 className="bold-white-outline text-center">
+                General Development
+              </h1>
               <button className="nes-btn is-primary">Languages</button>
               <div className="grid grid-cols-2 sm:grid-cols-3 gap-4 mt-6">
-                <span className="nes-badge"><span className="is-dark">Java</span></span>
-                <span className="nes-badge"><span className="is-warning">C</span></span>
+                <span className="nes-badge">
+                  <span className="is-dark">Java</span>
+                </span>
+                <span className="nes-badge">
+                  <span className="is-warning">C</span>
+                </span>
               </div>
               <button className="nes-btn is-primary">Concepts</button>
               <div className="grid grid-cols-2 sm:grid-cols-3 gap-4 mt-6">
-                <span className="nes-badge"><span className="is-success">OOPs</span></span>
-                <span className="nes-badge"><span className="is-error">DBMS</span></span>
+                <span className="nes-badge">
+                  <span className="is-success">OOPs</span>
+                </span>
+                <span className="nes-badge">
+                  <span className="is-error">DBMS</span>
+                </span>
               </div>
             </div>
           </div>
@@ -144,16 +221,32 @@ export default function Home() {
         <div className="nes-container is-rounded bg-[#b3b3ff] w-full max-w-[40rem]">
           <div className="p-6 space-y-6">
             <div className="w-full flex justify-center">
-              <h1 className="bold-white-outline text-center">Developer Tools</h1>
+              <h1 className="bold-white-outline text-center">
+                Developer Tools
+              </h1>
             </div>
             <div className="grid grid-cols-2 sm:grid-cols-3 gap-4 justify-items-center mt-6">
-              <a href="#" className="nes-badge"><span className="is-dark">VS.Code</span></a>
-              <a href="#" className="nes-badge"><span className="is-primary">PyCharm</span></a>
-              <a href="#" className="nes-badge"><span className="is-success">Git Bash</span></a>
-              <a href="#" className="nes-badge"><span className="is-warning">GoogleColab</span></a>
-              <a href="#" className="nes-badge"><span className="is-error">JupyterNtbk</span></a>
-              <a href="#" className="nes-badge"><span className="is-dark">Programming</span></a>
-              <a href="#" className="nes-badge"><span className="is-success">MERN</span></a>
+              <a href="#" className="nes-badge">
+                <span className="is-dark">VS.Code</span>
+              </a>
+              <a href="#" className="nes-badge">
+                <span className="is-primary">PyCharm</span>
+              </a>
+              <a href="#" className="nes-badge">
+                <span className="is-success">Git Bash</span>
+              </a>
+              <a href="#" className="nes-badge">
+                <span className="is-warning">GoogleColab</span>
+              </a>
+              <a href="#" className="nes-badge">
+                <span className="is-error">JupyterNtbk</span>
+              </a>
+              <a href="#" className="nes-badge">
+                <span className="is-dark">Programming</span>
+              </a>
+              <a href="#" className="nes-badge">
+                <span className="is-success">MERN</span>
+              </a>
             </div>
           </div>
         </div>
@@ -162,18 +255,71 @@ export default function Home() {
       {/* Social Section */}
       <div className="w-full flex justify-center mt-20">
         <div className="nes-container is-dark with-title relative bg-black text-white w-full max-w-[40rem] min-h-[30rem]">
-          <p className="title text-center text-xs sm:text-base">Connect With Me</p>
+          <p className="title text-center text-xs sm:text-base">
+            Connect With Me
+          </p>
           <div className="flex flex-col items-center justify-center h-full">
             <div className="grid grid-cols-2 sm:grid-cols-3 gap-x-6 gap-y-12 justify-items-center p-6">
-              <a href="https://github.com/Chauhan-Ujjawal" target="_blank"><Image src="/github.gif" alt="GitHub" width={100} height={100} className="hover:scale-110 transition-transform" /></a>
-              <a href="https://www.linkedin.com/in/ujjawal-chauhan01" target="_blank"><i className="nes-icon linkedin is-large"></i></a>
-              <a href="https://www.geeksforgeeks.org/user/ujjava0kfg/" target="_blank"><Image src="/gfg.png" alt="GFG" width={75} height={75} className="hover:scale-110 transition-transform" /></a>
-              <a href="https://leetcode.com/u/chauhan_ujjawal_" target="_blank"><Image src="/leetcode.png" alt="LeetCode" width={110} height={110} className="hover:scale-110 transition-transform" /></a>
-              <a href="https://www.behance.net/ujjawalchauhan06" target="_blank"><Image src="/behance.png" alt="Behance" width={90} height={90} className="hover:scale-110 transition-transform" /></a>
-              <a href="https://www.kaggle.com/ujjawalchauhann" target="_blank"><Image src="/kaggle.png" alt="Kaggle" width={90} height={90} className="hover:scale-110 transition-transform" /></a>
+              <a href="https://github.com/Chauhan-Ujjawal" target="_blank">
+                <Image
+                  src="/github.gif"
+                  alt="GitHub"
+                  width={100}
+                  height={100}
+                  className="hover:scale-110 transition-transform"
+                />
+              </a>
+              <a
+                href="https://www.linkedin.com/in/ujjawal-chauhan01"
+                target="_blank"
+              >
+                <i className="nes-icon linkedin is-large"></i>
+              </a>
+              <a
+                href="https://www.geeksforgeeks.org/user/ujjava0kfg/"
+                target="_blank"
+              >
+                <Image
+                  src="/gfg.png"
+                  alt="GFG"
+                  width={75}
+                  height={75}
+                  className="hover:scale-110 transition-transform"
+                />
+              </a>
+              <a href="https://leetcode.com/u/chauhan_ujjawal_" target="_blank">
+                <Image
+                  src="/leetcode.png"
+                  alt="LeetCode"
+                  width={110}
+                  height={110}
+                  className="hover:scale-110 transition-transform"
+                />
+              </a>
+              <a
+                href="https://www.behance.net/ujjawalchauhan06"
+                target="_blank"
+              >
+                <Image
+                  src="/behance.png"
+                  alt="Behance"
+                  width={90}
+                  height={90}
+                  className="hover:scale-110 transition-transform"
+                />
+              </a>
+              <a href="https://www.kaggle.com/ujjawalchauhann" target="_blank">
+                <Image
+                  src="/kaggle.png"
+                  alt="Kaggle"
+                  width={90}
+                  height={90}
+                  className="hover:scale-110 transition-transform"
+                />
+              </a>
             </div>
           </div>
-          <img
+          <Image
             src="/kittycat.gif"
             height={150}
             width={150}
